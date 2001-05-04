43a0c49199fc14263b85215e39ed01ef6a9e4367
lorens <hlsyounes@gmail.com>
2001-05-04 03:51:21 +0000

    Initial revision

---
+++ flaws.cc
@@ -0,0 +1,13 @@
+#include "plans.h"
+
+
+/* Prints this open condition on the given stream. */
+void OpenCondition::print(ostream& os) const {
+  os << "#<OPEN " << condition << " step " << step_id << ">";
+}
+
+
+/* Prints this threatened causal link on the given stream. */
+void Unsafe::print(ostream& os) const {
+  os << "#<UNSAFE " << link << " step " << step_id << ">";
+}
