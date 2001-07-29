607aa2d6cae5ac7d056c1c60e910f81fecb24447
lorens <lorens@414ba85e-7e40-11de-ae7d-69a7100e5098>
2001-07-29 17:59:35 +0000

    Added a few more options and now catches Unimplemented exception.

--- trunk/vhpop.cc
+++ trunk/vhpop.cc
@@ -1,7 +1,7 @@
 /*
  * Main program.
  *
- * $Id: vhpop.cc,v 1.2 2001-05-04 04:06:14 lorens Exp $
+ * $Id: vhpop.cc,v 1.3 2001-07-29 17:59:35 lorens Exp $
  */
 #include <iostream>
 #include <stdio.h>
@@ -18,7 +18,8 @@
 extern int yyparse();
 /* File to parse. */
 extern FILE* yyin;
-
+/* Level of warnings. */
+extern int warning_level;
 
 /* Program name. */
 static const string PROGRAM_NAME = "tpop";
@@ -30,8 +31,9 @@ static struct option long_options[] = {
   { "trans", no_argument, NULL, 't' },
   { "limit", required_argument, NULL, 'l' },
   { "verbose", optional_argument, NULL, 'v' },
-  { "help", no_argument, NULL, 0 },
-  { "version", no_argument, NULL, 1 },
+  { "help", no_argument, NULL, '?' },
+  { "version", no_argument, NULL, 'V' },
+  { "warnings", optional_argument, NULL, 'w' },
   { 0, 0, 0, 0 }
 };
 
@@ -41,24 +43,27 @@ static void display_help() {
   cout << PROGRAM_NAME << endl
        << "usage: " << PROGRAM_NAME << " [options] [file ...]" << endl
        << "options:" << endl
-       << "  -h h,  --heuristic=h\t"
-       << "use heuristic h;" << endl
-       << "\t\t\t  h can be `MAX' (default), `SUM', `SUMMAX', or `UCPOP'"
-       << endl
        << "  -e[n], --early[=n]\t"
        << "enable early linking of open conditions" << endl
-       << "  -t,    --trans\t"
-       << "enable transformational plan operators" << endl
+       << "  -h h,  --heuristic=h\t"
+       << "use heuristic h;" << endl
+       << "\t\t\t  h can be `MAX' (default), `SUM', or `UCPOP'" << endl
        << "  -l l,  --limit=l\t"
        << "search no more than l plans" << endl
+       << "  -t,    --trans\t"
+       << "enable transformational plan operators" << endl
        << "  -v[n], --verbose[=n]\t"
        << "use verbosity level n;" << endl
        << "\t\t\t  n is a number from 0 (verbose mode off) and up;" << endl
        << "\t\t\t  default level is 1 if optional argument is left out" << endl
-       << "          --help\t"
-       << "display this help and exit" << endl
-       << "          --version\t"
+       << "  -V     --version\t"
        << "display version information and exit" << endl
+       << "  -w[n], --warnings[=n]\t"
+       << "determines how warnings are treated;" << endl
+       << "\t\t\t  0 supresses warnings; 1 displays warnings;" << endl
+       << "\t\t\t  2 treats warnings as errors" << endl
+       << "  -?     --help\t\t"
+       << "display this help and exit" << endl
        << "  file ...\t\t"
        << "files containing domain and problem descriptions;" << endl
        << "\t\t\t  if none given, use standard input" << endl;
@@ -68,7 +73,7 @@ static void display_help() {
 /* Displays version information. */
 static void display_version() {
   cout << PROGRAM_NAME << endl
-       << " Written by Hakan Younes (lorens@cs.cmu.edu)." << endl;
+       << "Written by H\345kan L. Younes (lorens@cs.cmu.edu)." << endl;
 }
 
 
@@ -104,16 +109,13 @@ int main(int argc, char* argv[]) {
    */
   while (1) {
     int option_index = 0;
-    int c = getopt_long(argc, argv, "h:e::tl:v::",
+    int c = getopt_long(argc, argv, "e::h:l:tv::Vw::?",
 			long_options, &option_index);
     if (c == -1) {
       break;
     }
     switch (c) {
-    case 0:
-      display_help();
-      return 0;
-    case 1:
+    case 'V':
       display_version();
       return 0;
     case 'h':
@@ -121,8 +123,6 @@ int main(int argc, char* argv[]) {
 	heuristic = MAX_HEURISTIC;
       } else if (strcasecmp(optarg, "SUM") == 0) {
 	heuristic = SUM_HEURISTIC;
-      } else if (strcasecmp(optarg, "SUMMAX") == 0) {
-	heuristic = SUMMAX_HEURISTIC;
       } else if (strcasecmp(optarg, "UCPOP") == 0) {
 	heuristic = UCPOP_HEURISTIC;
       } else {
@@ -145,8 +145,15 @@ int main(int argc, char* argv[]) {
     case 'v':
       verbosity = (optarg != NULL) ? atoi(optarg) : 1;
       break;
+    case 'w':
+      warning_level = (optarg != NULL) ? atoi(optarg) : 1;
+      break;
     case ':':
     case '?':
+      if (optopt == '?') {
+	display_help();
+	return 0;
+      }
     default:
       cerr << "Try `" << PROGRAM_NAME << " --help' for more information."
 	   << endl;
@@ -154,84 +161,89 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  /*
-   * Read pddl files.
-   */
-  if (optind < argc) {
+  try {
     /*
-     * Use remaining command line arguments as file names.
+     * Read pddl files.
      */
-    while (optind < argc) {
-      if (!read_file(argv[optind++])) {
+    if (optind < argc) {
+      /*
+       * Use remaining command line arguments as file names.
+       */
+      while (optind < argc) {
+	if (!read_file(argv[optind++])) {
+	  return -1;
+	}
+      }
+    } else {
+      /*
+       * No remaining command line argument, so read from standard input.
+       */
+      yyin = stdin;
+      if (yyparse() != 0) {
 	return -1;
       }
     }
-  } else {
-    /*
-     * No remaining command line argument, so read from standard input.
-     */
-    yyin = stdin;
-    if (yyparse() != 0) {
-      return -1;
-    }
-  }
 
-  if (verbosity > 0) {
-    /*
-     * Display domains and problems.
-     */
-    cout << "----------------------------------------"<< endl;
-    cout << "domains:" << endl;
-    for (Domain::DomainMap::const_iterator i = Domain::begin();
-	 i != Domain::end(); i++) {
-      cout << endl << *(*i).second << endl;
+    if (verbosity > 0) {
+      /*
+       * Display domains and problems.
+       */
+      cout << "----------------------------------------"<< endl;
+      cout << "domains:" << endl;
+      for (Domain::DomainMap::const_iterator i = Domain::begin();
+	   i != Domain::end(); i++) {
+	cout << endl << *(*i).second << endl;
+      }
+      cout << "----------------------------------------"<< endl;
+      cout << "problems:" << endl;
+      for (Problem::ProblemMap::const_iterator i = Problem::begin();
+	   i != Problem::end(); i++) {
+	cout << endl << *(*i).second << endl;
+      }
+      cout << "----------------------------------------"<< endl;
     }
-    cout << "----------------------------------------"<< endl;
-    cout << "problems:" << endl;
+
+    /* Set output format for floating point numbers. */
+    cout.setf(ios::fixed, ios::floatfield);
+    cout.precision(3);
+
+    /*
+     * Solve the problems.
+     */
     for (Problem::ProblemMap::const_iterator i = Problem::begin();
 	 i != Problem::end(); i++) {
-      cout << endl << *(*i).second << endl;
-    }
-    cout << "----------------------------------------"<< endl;
-  }
-
-  /* Set output format for floating point numbers. */
-  cout.setf(ios::fixed, ios::floatfield);
-  cout.precision(3);
-
-  /*
-   * Solve the problems.
-   */
-  for (Problem::ProblemMap::const_iterator i = Problem::begin();
-       i != Problem::end(); i++) {
-    struct itimerval timer = { { 1000000, 900000 }, { 1000000, 900000 } };
-    const Problem& problem = *(*i).second;
-    setitimer(ITIMER_PROF, &timer, NULL);
-    const Plan* plan = Plan::plan(problem, heuristic, early_linking,
-				  transformations, limit, verbosity);
-    getitimer(ITIMER_PROF, &timer);
-    /* Planning time. */
-    double t = 1000000.9
-      - (timer.it_value.tv_sec + timer.it_value.tv_usec*1e-6);
-    if (t < 0.0) {
-      t = 0.0;
-    }
-    if (plan != NULL) {
-      if (plan->complete()) {
-	if (verbosity == 0) {
-	  cout << problem.name << ' ' << t << ' ' << *plan << endl;
-	} else {
-	  cout << endl << *plan << endl;
-	}
-      } else {
-	if (verbosity > 0) {
-	  cout << endl << *plan;
-	  cout << endl << "Search limit reached." << endl;
-	}
+      struct itimerval timer = { { 1000000, 900000 }, { 1000000, 900000 } };
+      const Problem& problem = *(*i).second;
+      setitimer(ITIMER_PROF, &timer, NULL);
+      const Plan* plan = Plan::plan(problem, heuristic, early_linking,
+				    transformations, limit, verbosity);
+      getitimer(ITIMER_PROF, &timer);
+      /* Planning time. */
+      double t = 1000000.9
+	- (timer.it_value.tv_sec + timer.it_value.tv_usec*1e-6);
+      if (t < 0.0) {
+	t = 0.0;
       }
-    } else if (verbosity > 0) {
-      cout << endl << "Problem has no solution." << endl;
+      if (plan != NULL) {
+	if (plan->complete()) {
+	  if (verbosity == 0) {
+	    cout << problem.name << ' ' << t << ' ' << *plan << endl;
+	  } else {
+	    cout << endl << *plan << endl;
+	  }
+	} else {
+	  if (verbosity > 0) {
+	    cout << endl << *plan;
+	    cout << endl << "Search limit reached." << endl;
+	  }
+	}
+      } else if (verbosity > 0) {
+	cout << endl << "Problem has no solution." << endl;
+      }
     }
+  } catch (const Unimplemented& ex) {
+    cerr << ex;
+    return -1;
   }
 
   return 0;
