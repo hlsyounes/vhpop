b924b9cef8a4c1dd538d680110cf2f05aa196f7a
lorens <lorens@414ba85e-7e40-11de-ae7d-69a7100e5098>
2001-07-29 18:12:02 +0000

    Added message to Unimplemented exception.
    Removed some string related functions that are not of general interest.

--- trunk/support.h
+++ trunk/support.h
@@ -2,14 +2,14 @@
 /*
  * Auxiliary types and functions.
  *
- * $Id: support.h,v 1.1 2001-05-03 15:23:43 lorens Exp $
+ * $Id: support.h,v 1.2 2001-07-29 18:12:02 lorens Exp $
  */
 #ifndef SUPPORT_H
 #define SUPPORT_H
 
-#include <hash_map>
+#include <iostream>
 #include <string>
-#include <ctype.h>
+#include <hash_map>
 #include <gc/gc_cpp.h>
 #include <gc/new_gc_alloc.h>
 
@@ -24,19 +24,31 @@ typedef single_client_traceable_alloc container_alloc;
  * Exception thrown for unimplemented features.
  */
 struct Unimplemented {
+  const string message;
+
+  Unimplemented(const string& message)
+    : message(message) {
+  }
+private:
+  void print(ostream& os) const;
+
+  friend ostream& operator<<(ostream& os, const Unimplemented& ex);
 };
+
+/* Output operator for unimplemented exception. */
+inline ostream& operator<<(ostream& os, const Unimplemented& ex) {
+  ex.print(os);
+  return os;
+}
 
 
 /*
- * Hash function for string objects.
+ * Hash function object for strings.
  */
 struct hash<string> {
   size_t operator()(const string& s) const {
-    return h(s.c_str());
+    return hash<char*>()(s.c_str());
   }
-
-private:
-  hash<char*> h;
 };
 
 
@@ -80,33 +92,5 @@ struct Chain : public gc {
     }
   }
 };
-
-
-/*
- * Converts a string to lowercase.
- */
-inline string tolower(const char* s) {
-  string result;
-  for (const char* p = s; *p != '\0'; p++) {
-    result += tolower(*p);
-  }
-  return result;
-}
-
-/*
- * Converts an integer to a string.
- */
-inline string tostring(unsigned int n) {
-  string result;
-  while (n > 0) {
-    result = char(n % 10 + '0') + result;
-    n /= 10;
-  }
-  if (result.length() == 0) {
-    return "0";
-  } else {
-    return result;
-  }
-}
 
 #endif /* SUPPORT_H */
