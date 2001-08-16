50f0022178e439f766398693cb8df4a29f5891d2
lorens <hlsyounes@gmail.com>
2001-08-16 00:39:06 +0000

    *** empty log message ***

--- domains.h
+++ domains.h
@@ -2,7 +2,7 @@
 /*
  * Domain descriptions.
  *
- * $Id: domains.h,v 1.9 2001-08-12 16:32:25 lorens Exp $
+ * $Id: domains.h,v 1.10 2001-08-16 00:39:01 lorens Exp $
  */
 #ifndef DOMAINS_H
 #define DOMAINS_H
@@ -134,22 +134,10 @@ struct EffectList : public gc, vector<const Effect*, container_alloc> {
   }
 
   /* Returns an instantiation of this effect list. */
-  const EffectList& instantiation(size_t id) const {
-    EffectList& effects = *(new EffectList());
-    for (const_iterator i = begin(); i != end(); i++) {
-      effects.push_back(&(*i)->instantiation(id));
-    }
-    return effects;
-  }
+  const EffectList& instantiation(size_t id) const;
 
   /* Returns this effect list subject to the given substitutions. */
-  const EffectList& substitution(const SubstitutionList& subst) const {
-    EffectList& effects = *(new EffectList());
-    for (const_iterator i = begin(); i != end(); i++) {
-      effects.push_back(&(*i)->substitution(subst));
-    }
-    return effects;
-  }
+  const EffectList& substitution(const SubstitutionList& subst) const;
 
   /* Fills the provided list with goals achievable by the effect in
      this list. */
@@ -201,20 +189,25 @@ protected:
   /* Constructs an action. */
   Action(const Formula& precondition, const EffectList& effects);
 
+  /* Prints this action on the given stream. */
+  virtual void print(ostream& os) const = 0;
+
   /* Tests if this action equals the given action. */
   virtual bool equals(const Action& a) const = 0;
 
   /* Returns the hash value of this action. */
   virtual size_t hash_value() const = 0;
 
-  /* Prints this action on the given stream. */
-  virtual void print(ostream& os) const = 0;
-
-private:
+  friend ostream& operator<<(ostream& os, const Action& a);
   friend bool operator==(const Action& a1, const Action& a2);
   friend struct hash<Action>;
-  friend ostream& operator<<(ostream& os, const Action& a);
 };
+
+/* Output operator for actions. */
+inline ostream& operator<<(ostream& os, const Action& a) {
+  a.print(os);
+  return os;
+}
 
 /* Equality operator for actions. */
 inline bool operator==(const Action& a1, const Action& a2) {
@@ -227,20 +220,20 @@ inline bool operator!=(const Action& a1, const Action& a2) {
 }
 
 /*
- * Equality function object for action pointers.
- */
-struct equal_to<const Action*> {
-  bool operator()(const Action* a1, const Action* a2) const {
-    return *a1 == *a2;
-  }
-};
-
-/*
  * Hash function object for actions.
  */
 struct hash<Action> {
   size_t operator()(const Action& a) const {
     return a.hash_value();
+  }
+};
+
+/*
+ * Equality function object for action pointers.
+ */
+struct equal_to<const Action*> {
+  bool operator()(const Action* a1, const Action* a2) const {
+    return *a1 == *a2;
   }
 };
 
@@ -252,12 +245,6 @@ struct hash<const Action*> {
     return hash<Action>()(*a);
   }
 };
-
-/* Output operator for actions. */
-inline ostream& operator<<(ostream& os, const Action& a) {
-  a.print(os);
-  return os;
-}
 
 
 /*
@@ -299,19 +286,14 @@ struct ActionSchema : public Action {
 			      const Problem& problem) const;
 
 protected:
-  /* Tests if this action equals the given action. */
-  virtual bool equals(const Action& a) const {
-    const ActionSchema* as = dynamic_cast<const ActionSchema*>(&a);
-    return as != NULL && name == as->name;
-  }
-
-  /* Returns the hash value of this action. */
-  virtual size_t hash_value() const {
-    return hash<string>()(name);
-  }
-
   /* Prints this action on the given stream. */
   virtual void print(ostream& os) const;
+
+  /* Tests if this action equals the given action. */
+  virtual bool equals(const Action& a) const;
+
+  /* Returns the hash value of this action. */
+  virtual size_t hash_value() const;
 };
 
 
@@ -324,26 +306,22 @@ struct GroundAction : public Action {
 	       const Formula& precondition, const EffectList& effects);
 
   /* Returns a formula representing this action. */
-  virtual const AtomicFormula& action_formula(size_t id) const {
-    return formula;
-  }
+  virtual const AtomicFormula& action_formula(size_t id) const;
 
   /* Fills the provided action list with all instantiations of this
      action. */
   virtual void instantiations(ActionList& actions,
-			      const Problem& problem) const {
-    actions.push_back(this);
-  }
+			      const Problem& problem) const;
 
 protected:
+  /* Prints this action on the given stream. */
+  virtual void print(ostream& os) const;
+
   /* Tests if this action equals the given action. */
   virtual bool equals(const Action& a) const;
 
   /* Returns the hash value of this action. */
   virtual size_t hash_value() const;
-
-  /* Prints this action on the given stream. */
-  virtual void print(ostream& os) const;
 
 private:
   /* Atomic formula representing this ground action. */
