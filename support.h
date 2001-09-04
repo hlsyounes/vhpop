bcaeebc66b0b2a8a7bc9b4ba3b8647cf41314c04
lorens <hlsyounes@gmail.com>
2001-09-04 18:19:14 +0000

    Moved chain class to separate file.

--- support.h
+++ support.h
@@ -2,7 +2,7 @@
 /*
  * Auxiliary types and functions.
  *
- * $Id: support.h,v 1.3 2001-08-10 04:08:18 lorens Exp $
+ * $Id: support.h,v 1.4 2001-09-04 18:19:14 lorens Exp $
  */
 #ifndef SUPPORT_H
 #define SUPPORT_H
@@ -52,51 +52,6 @@ struct hash<string> {
   /* Hash function for strings. */
   size_t operator()(const string& s) const {
     return hash<char*>()(s.c_str());
-  }
-};
-
-
-/*
- * Template chain class.
- */
-template<class T>
-struct Chain : public gc {
-  T head;
-  const Chain<T>* tail;
-
-  /* Constructs a chain with the given head and tail. */
-  Chain<T>(const T& h, const Chain<T>* t)
-    : head(h), tail(t) {
-  }
-
-  /* Returns the size of this chain. */
-  size_t size() const {
-    size_t result = 0;
-    for (const Chain<T>* ci = this; ci != NULL; ci = ci->tail) {
-      result++;
-    }
-    return result;
-  }
-
-  /* Checks if this chain contains the given element. */
-  bool contains(const T& h) const {
-    for (const Chain<T>* ci = this; ci != NULL; ci = ci->tail) {
-      if (h == ci->head) {
-	return true;
-      }
-    }
-    return false;
-  }
-
-  /* Returns a chain with the first occurance of the given element removed. */
-  const Chain<T>* remove(const T& h) const {
-    if (h == head) {
-      return tail;
-    } else if (tail != NULL) {
-      return new Chain<T>(head, tail->remove(h));
-    } else {
-      return this;
-    }
   }
 };
 
