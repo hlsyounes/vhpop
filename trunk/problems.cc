7ebfeb13212332fc300c7085c3f11da21fef3965
lorens <lorens@414ba85e-7e40-11de-ae7d-69a7100e5098>
2001-10-06 23:29:00 +0000

    *** empty log message ***

--- trunk/problems.cc
+++ trunk/problems.cc
@@ -1,5 +1,5 @@
 /*
- * $Id: problems.cc,v 1.3 2001-08-11 06:15:30 lorens Exp $
+ * $Id: problems.cc,v 1.4 2001-10-06 23:28:57 lorens Exp $
  */
 #include "problems.h"
 #include "domains.h"
@@ -13,8 +13,8 @@ Problem::ProblemMap Problem::problems = Problem::ProblemMap();
 /* Returns the object with the given name, or NULL if it is
    undefined. */
 const Name* Problem::find_object(const string& name) const {
-  NameMap::const_iterator i = objects.find(name);
-  return (i != objects.end()) ? (*i).second : NULL;
+  NameMapIter ni = objects.find(name);
+  return (ni != objects.end()) ? (*ni).second : NULL;
 }
 
 
@@ -22,9 +22,9 @@ const Name* Problem::find_object(const string& name) const {
    declared in the domain) that are compatible with the given type. */
 void Problem::compatible_objects(NameList& objects, const Type& t) const {
   domain.compatible_constants(objects, t);
-  for (NameMap::const_iterator i = this->objects.begin();
-       i != this->objects.end(); i++) {
-    const Name& name = *(*i).second;
+  for (NameMapIter ni = this->objects.begin();
+       ni != this->objects.end(); ni++) {
+    const Name& name = *(*ni).second;
     if (name.type.subtype(t)) {
       objects.push_back(&name);
     }
@@ -35,22 +35,22 @@ void Problem::compatible_objects(NameList& objects, const Type& t) const {
 /* Fills the provided action list with ground actions instantiated
    from the action schemas of the domain. */
 void Problem::instantiated_actions(ActionList& actions) const {
-  for (ActionMap::const_iterator i = domain.actions.begin();
-       i != domain.actions.end(); i++) {
-    (*i).second->instantiations(actions, *this);
+  for (ActionMapIter ai = domain.actions.begin();
+       ai != domain.actions.end(); ai++) {
+    (*ai).second->instantiations(actions, *this);
   }
 }
 
 
-/* Prints this problem on the given stream. */
+/* Prints this object on the given stream. */
 void Problem::print(ostream& os) const {
   os << "name: " << name;
   os << endl << "domain: " << domain.name;
   os << endl << "objects:";
-  for (NameMap::const_iterator i = objects.begin(); i != objects.end(); i++) {
-    os << ' ' << *(*i).second;
-    if (!(*i).second->type.object()) {
-      os << " - " << (*i).second->type;
+  for (NameMapIter ni = objects.begin(); ni != objects.end(); ni++) {
+    os << ' ' << *(*ni).second;
+    if (!(*ni).second->type.object()) {
+      os << " - " << (*ni).second->type;
     }
   }
   if (init != NULL) {
