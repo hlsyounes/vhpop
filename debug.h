44a01dae24d99e9494ae560cefd74f442795f0af
lorens <hlsyounes@gmail.com>
2001-10-10 13:41:18 +0000

    Initial revision

---
+++ debug.h
@@ -0,0 +1,13 @@
+/* -*-C++-*- */
+/*
+ * Debugging related stuff.
+ *
+ * $Id: debug.h,v 1.1 2001-10-10 13:41:18 lorens Exp $
+ */
+#ifndef DEBUG_H
+#define DEBUG_H
+
+/* Verbosity level. */
+extern int verbosity;
+
+#endif /* DEBUG_H */
