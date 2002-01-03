6a4b3d15347b70e6c93217e556b7d64bf62e0bfb
lorens <lorens@414ba85e-7e40-11de-ae7d-69a7100e5098>
2002-01-03 12:40:15 +0000

    *** empty log message ***

--- trunk/parameters.h
+++ trunk/parameters.h
@@ -2,13 +2,15 @@
 /*
  * Planning parameters.
  *
- * $Id: parameters.h,v 1.4 2001-12-30 16:16:12 lorens Exp $
+ * $Id: parameters.h,v 1.5 2002-01-03 12:40:15 lorens Exp $
  */
 #ifndef PARAMETERS_H
 #define PARAMETERS_H
 
 #include "support.h"
-#include "heuristics.h"
+
+struct Heuristic;
+struct FlawSelectionOrder;
 
 
 /*
@@ -33,18 +35,18 @@ struct Parameters : public gc {
   size_t time_limit;
   /* Search algorithm to use. */
   SearchAlgorithm search_algorithm;
-  /* Whether to use ground actions. */
-  bool ground_actions;
-  /* Whether to use transformational planner. */
-  bool transformational;
   /* Plan selection heuristic. */
-  Heuristic heuristic;
+  Heuristic& heuristic;
   /* Weight to use with heuristic. */
   double weight;
   /* Flaw selecion order. */
-  FlawSelectionOrder flaw_order;
+  FlawSelectionOrder& flaw_order;
+  /* Whether to use ground actions. */
+  bool ground_actions;
   /* Whether to use parameter domain constraints. */
   bool domain_constraints;
+  /* Whether to use transformational planner. */
+  bool transformational;
 
   /* Constructs default planning parameters. */
   Parameters();
