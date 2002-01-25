387273fe1c9befeda848ed62e08f1b31f0c54307
lorens <hlsyounes@gmail.com>
2002-01-25 17:49:52 +0000

    *** empty log message ***

--- chain.h
+++ chain.h
@@ -2,7 +2,21 @@
 /*
  * Template chain class.
  *
- * $Id: chain.h,v 1.3 2001-12-27 19:59:49 lorens Exp $
+ * Copyright (C) 2002 Carnegie Mellon University
+ * Written by Håkan L. S. Younes.
+ *
+ * Permission is hereby granted to distribute this software for
+ * non-commercial research purposes, provided that this copyright
+ * notice is included with any such distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+ * SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
+ * ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ *
+ * $Id: chain.h,v 1.4 2002-01-25 17:49:52 lorens Exp $
  */
 #ifndef CHAIN_H
 #define CHAIN_H
