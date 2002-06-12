20659945b0a7e9c39be7bb01e0684818eb719612
lorens <lorens@414ba85e-7e40-11de-ae7d-69a7100e5098>
2002-06-12 23:05:05 +0000

    *** empty log message ***

--- trunk/chain.h
+++ trunk/chain.h
@@ -16,7 +16,7 @@
  * SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
  * ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
  *
- * $Id: chain.h,v 3.3 2002-03-29 10:04:30 lorens Exp $
+ * $Id: chain.h,v 3.4 2002-06-12 23:05:05 lorens Exp $
  */
 #ifndef CHAIN_H
 #define CHAIN_H
@@ -24,62 +24,8 @@
 #include "debug.h"
 
 
-/*
- * Template chain class.
- */
-template<typename T>
-struct Chain {
-  T head;
-  const Chain<T>* tail;
-
-  /* Constructs a chain with the given head and tail. */
-  Chain<T>(const T& h, const Chain<T>* t)
-    : head(h), tail(t) {
-  }
-
-  /* Returns the size of this chain. */
-  size_t size() const {
-    size_t result = 0;
-    for (const Chain<T>* ci = this; ci != NULL; ci = ci->tail) {
-      result++;
-    }
-    return result;
-  }
-
-  /* Checks if this chain contains the given element. */
-  bool contains(const T& h) const {
-    for (const Chain<T>* ci = this; ci != NULL; ci = ci->tail) {
-      if (h == ci->head) {
-	return true;
-      }
-    }
-    return false;
-  }
-
-  /* Returns a chain with the first occurance of the given element removed. */
-  const Chain<T>* remove(const T& h) const {
-    if (h == head) {
-      return tail;
-    } else if (tail != NULL) {
-      Chain<T>* prev = new Chain<T>(head, NULL);
-      const Chain<T>* top = prev;
-      for (const Chain<T>* ci = tail; ci != NULL; ci = ci->tail) {
-	if (h == ci->head) {
-	  prev->tail = ci->tail;
-	  break;
-	} else {
-	  Chain<T>* tmp = new Chain<T>(ci->head, NULL);
-	  prev->tail = tmp;
-	  prev = tmp;
-	}
-      }
-      return top;
-    } else {
-      return this;
-    }
-  }
-};
-
+/* ====================================================================== */
+/* CollectibleChain */
 
 /*
  * Template chain class.
