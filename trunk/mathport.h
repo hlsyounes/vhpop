1f0c2048cc8c7a015f455f7899cb698bbdc0a5f3
lorens <lorens@414ba85e-7e40-11de-ae7d-69a7100e5098>
2002-07-16 22:25:38 +0000

    Initial revision

---
+++ trunk/mathport.h
@@ -0,0 +1,37 @@
+/* -*-C++-*- */
+/*
+ * Math library portability support.
+ *
+ * Copyright (C) 2002 Carnegie Mellon University
+ * Written by Håkan L. S. Younes.
+ *
+ * Permission is hereby granted to distribute this software for
+ * non-commercial research purposes, provided that this copyright
+ * notice is included with any such distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
+ * EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ * PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
+ * SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
+ * ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ * 
+ * $$
+ */
+#ifndef MATHCOMP_H
+#define MATHPORT_H
+
+#define __USE_ISOC99 1
+#define __USE_ISOC9X 1
+#include <cmath>
+
+
+#ifndef INFINITY
+#define INFINITY 1.0f/0.0f;
+#endif
+
+#if !HAVE_ISINF
+#define isinf(x) (!(isnan(x) || finite(x)))
+#endif
+
+#endif /* MATHCOMP_H */
