15fca7a83824d607f80aefbed8cdf7b393d3cff0
lorens <lorens@414ba85e-7e40-11de-ae7d-69a7100e5098>
2003-08-24 23:17:55 +0000

    Moved action definitions to separate file.

--- trunk/domains.h
+++ trunk/domains.h
@@ -16,205 +16,19 @@
  * SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
  * ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
  *
- * $Id: domains.h,v 6.3 2003-07-28 21:31:48 lorens Exp $
+ * $Id: domains.h,v 6.4 2003-08-24 23:17:40 lorens Exp $
  */
 #ifndef DOMAINS_H
 #define DOMAINS_H
 
 #include <config.h>
 #include "requirements.h"
-#include "effects.h"
-#include "formulas.h"
+#include "actions.h"
 #include "terms.h"
 #include "predicates.h"
 #include "types.h"
 #include "hashing.h"
 #include <map>
-
-struct Problem;
-
-
-/* ====================================================================== */
-/* Action */
-
-/*
- * Abstract action definition.
- */
-struct Action {
-  /* Deletes this action. */
-  virtual ~Action();
-
-  /* Sets the condition for this action. */
-  void set_condition(const Condition& condition);
-
-  /* Adds an effect to this action. */
-  void add_effect(const Effect& effect);
-
-  /* Sets the minimum duration for this action. */
-  void set_min_duration(double min_duration);
-
-  /* Sets the maximum duration for this action. */
-  void set_max_duration(double max_duration);
-
-  /* Sets the duration for this action. */
-  void set_duration(double duration);
-
-  /* Returns the name of this action. */
-  const std::string& name() const { return name_; }
-
-  /* Return the condition of this action. */
-  const Condition& condition() const { return *condition_; }
-
-  /* List of action effects. */
-  const EffectList& effects() const { return effects_; }
-
-  /* Whether this is a durative action. */
-  bool durative() const { return durative_; }
-
-  /* Minimum duration of this action. */
-  float min_duration() const { return min_duration_; }
-
-  /* Maximum duration of this action. */
-  float max_duration() const { return max_duration_; }
-
-  /* Fills the provided sets with predicates achievable by this
-     action. */
-  void achievable_predicates(PredicateSet& preds,
-			     PredicateSet& neg_preds) const;
-
-  /* "Strengthens" the effects of this action. */
-  void strengthen_effects();
-
-  /* Prints this action on the given stream with the given bindings. */
-  virtual void print(std::ostream& os, const TermTable& terms,
-		     size_t step_id, const Bindings& bindings) const = 0;
-
-protected:
-  /* Constructs an action with the given name. */
-  Action(const std::string& name, bool durative);
-
-private:
-  /* Name of this action. */
-  std::string name_;
-  /* Action condition. */
-  const Condition* condition_;
-  /* List of action effects. */
-  EffectList effects_;
-  /* Whether this is a durative action. */
-  bool durative_;
-  /* Minimum duration of this action. */
-  float min_duration_;
-  /* Maximum duration of this action. */
-  float max_duration_;
-};
-
-
-/* ====================================================================== */
-/* ActionList */
-
-/*
- * List of action definitions.
- */
-struct ActionList : public std::vector<const Action*> {
-};
-
-/* Iterator for action lists. */
-typedef ActionList::const_iterator ActionListIter;
-
-
-/* ====================================================================== */
-/* ActionSchema */
-
-struct GroundAction;
-struct GroundActionList;
-struct Domain;
-
-/*
- * Action schema definition.
- */
-struct ActionSchema : public Action {
-  /* Constructs an action schema with the given name. */
-  ActionSchema(const std::string& name, bool durative);
-
-  /* Adds a parameter to this action schema. */
-  void add_parameter(Variable var);
-
-  /* Returns the parameters of this action schema. */
-  const VariableList& parameters() const { return parameters_; }
-
-  /* Fills the provided action list with all instantiations of this
-     action schema. */
-  void instantiations(GroundActionList& actions, const Problem& problem) const;
-
-  /* Prints this action on the given stream. */
-  void print(std::ostream& os, const PredicateTable& predicates,
-	     const TermTable& terms) const;
-
-  /* Prints this action on the given stream with the given bindings. */
-  virtual void print(std::ostream& os, const TermTable& terms,
-		     size_t step_id, const Bindings& bindings) const;
-
-private:
-  /* Action schema parameters. */
-  VariableList parameters_;
-
-  /* Returns an instantiation of this action schema. */
-  const GroundAction* instantiation(const SubstitutionMap& args,
-				    const Problem& problem,
-				    const Condition& condition) const;
-};
-
-
-/* ====================================================================== */
-/* ActionSchemaMap */
-
-/*
- * Table of action schema definitions.
- */
-struct ActionSchemaMap : public std::map<std::string, const ActionSchema*> {
-};
-
-/* Iterator for action schema table. */
-typedef ActionSchemaMap::const_iterator ActionSchemaMapIter;
-
-
-/* ====================================================================== */
-/* GroundAction */
-
-/*
- * Ground action.
- */
-struct GroundAction : public Action {
-  /* Constructs a ground action with the given name. */
-  GroundAction(const std::string& name, bool durative);
-
-  /* Adds an argument to this ground action. */
-  void add_argument(Object arg);
-
-  /* Action arguments. */
-  const ObjectList& arguments() const { return arguments_; }
-
-  /* Prints this action on the given stream with the given bindings. */
-  virtual void print(std::ostream& os, const TermTable& terms,
-		     size_t step_id, const Bindings& bindings) const;
-
-private:
-  /* Action arguments. */
-  ObjectList arguments_;
-};
-
-
-/* ====================================================================== */
-/* GroundActionList */
-
-/*
- * List of ground actions.
- */
-struct GroundActionList : public std::vector<const GroundAction*> {
-};
-
-/* Iterator for ground action lists. */
-typedef GroundActionList::const_iterator GroundActionListIter;
 
 
 /* ====================================================================== */
@@ -228,17 +42,14 @@ struct Domain {
   struct DomainMap : public std::map<std::string, const Domain*> {
   };
 
-  /* Iterator for domain tables. */
-  typedef DomainMap::const_iterator DomainMapIter;
-
   /* Requirements for this domain. */
   Requirements requirements;
 
   /* Returns a const_iterator pointing to the first domain. */
-  static DomainMapIter begin();
+  static DomainMap::const_iterator begin();
 
   /* Returns a const_iterator pointing beyond the last domain. */
-  static DomainMapIter end();
+  static DomainMap::const_iterator end();
 
   /* Returns the domain with the given name, or NULL it is undefined. */
   static const Domain* find(const std::string& name);
@@ -256,13 +67,19 @@ struct Domain {
   const std::string& name() const { return name_; }
 
   /* Domain actions. */
-  const ActionSchemaMap& actions() const;
+  const ActionSchemaMap& actions() const { return actions_; }
 
   /* Returns the type table of this domain. */
-  TypeTable& types() const { return types_; }
+  TypeTable& types() { return types_; }
+
+  /* Returns the type table of this domain. */
+  const TypeTable& types() const { return types_; }
 
   /* Returns the predicate table of this domain. */
-  PredicateTable& predicates() const { return predicates_; }
+  PredicateTable& predicates() { return predicates_; }
+
+  /* Returns the predicate table of this domain. */
+  const PredicateTable& predicates() const { return predicates_; }
 
   /* Returns the term table of this domain. */
   TermTable& terms() { return terms_; }
@@ -288,9 +105,9 @@ private:
   /* Name of this domain. */
   std::string name_;
   /* Domain types. */
-  mutable TypeTable types_;
+  TypeTable types_;
   /* Domain predicates. */
-  mutable PredicateTable predicates_;
+  PredicateTable predicates_;
   /* Domain terms. */
   TermTable terms_;
   /* Domain action schemas. */
