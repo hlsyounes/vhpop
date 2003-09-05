fcdc522eeac5f5b2f74892a685030bd5e69ae6b6
lorens <lorens@414ba85e-7e40-11de-ae7d-69a7100e5098>
2003-09-05 16:31:04 +0000

    Added support for numeric initial conditions and recording of optimization metric.

--- trunk/problems.cc
+++ trunk/problems.cc
@@ -13,7 +13,7 @@
  * SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
  * ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
  *
- * $Id: problems.cc,v 6.6 2003-08-28 15:34:25 lorens Exp $
+ * $Id: problems.cc,v 6.7 2003-09-05 16:31:04 lorens Exp $
  */
 #include "problems.h"
 #include "domains.h"
@@ -61,8 +61,10 @@ void Problem::clear() {
 /* Constructs a problem. */
 Problem::Problem(const std::string& name, const Domain& domain)
   : name_(name), domain_(&domain), terms_(TermTable(domain.terms())),
-    init_action_(GroundAction("", false)), goal_(&Formula::TRUE) {
+    init_action_(GroundAction("", false)), goal_(&Formula::TRUE),
+    metric_(new Value(0)) {
   Formula::register_use(goal_);
+  Expression::register_use(metric_);
   const Problem* p = find(name);
   if (p != NULL) {
     delete p;
@@ -74,7 +76,12 @@ Problem::Problem(const std::string& name, const Domain& domain)
 /* Deletes a problem. */
 Problem::~Problem() {
   problems.erase(name());
+  for (ValueMap::const_iterator vi = init_values_.begin();
+       vi != init_values_.end(); vi++) {
+    Expression::unregister_use((*vi).first);
+  }
   Formula::unregister_use(goal_);
+  Expression::unregister_use(metric_);
   for (std::map<Type, const ObjectList*>::const_iterator oi =
 	 compatible_.begin(); oi != compatible_.end(); oi++) {
     delete (*oi).second;
@@ -90,6 +97,18 @@ void Problem::add_init_atom(const Atom& atom) {
 }
 
 
+/* Adds a function application value to the initial conditions of
+   this problem. */
+void Problem::add_init_value(const Application& application, float value) {
+  if (init_values_.find(&application) == init_values_.end()) {
+    init_values_.insert(std::make_pair(&application, value));
+    Expression::register_use(&application);
+  } else {
+    init_values_[&application] = value;
+  }
+}
+
+
 /* Sets the goal of this problem. */
 void Problem::set_goal(const Formula& goal) {
   if (goal_ != &goal) {
@@ -97,6 +116,35 @@ void Problem::set_goal(const Formula& goal) {
     goal_ = &goal;
     Formula::register_use(goal_);
   }
+}
+
+
+/* Sets the metric to minimize for this problem. */
+void Problem::set_metric(const Expression& metric, bool negate) {
+  const Expression* real_metric;
+  if (negate) {
+    real_metric = &Subtraction::make(*new Value(0), metric);
+  } else {
+    real_metric = &metric;
+  }
+  const Expression& inst_metric =
+    real_metric->instantiation(SubstitutionMap(), *this);
+  if (&inst_metric != real_metric) {
+    Expression::register_use(real_metric);
+    Expression::unregister_use(real_metric);
+    real_metric = &inst_metric;
+  }
+  if (real_metric != metric_) {
+    Expression::unregister_use(metric_);
+    metric_ = real_metric;
+    Expression::register_use(metric_);
+  }
+}
+
+
+/* Tests if the metric is constant. */
+bool Problem::constant_metric() const {
+  return typeid(metric()) == typeid(Value);
 }
 
 
@@ -155,14 +203,22 @@ std::ostream& operator<<(std::ostream& os, const Problem& p) {
     os << " - ";
     p.domain().types().print_type(os, p.terms().type(i));
   }
-  os << std::endl << "initial condition: ";
+  os << std::endl << "init: ";
   AtomSet::const_iterator ai = p.init_atoms().begin();
   (*ai)->print(os, p.domain().predicates(), p.terms(), 0, Bindings::EMPTY);
   for (ai++; ai != p.init_atoms().end(); ai++) {
     os << ' ';
     (*ai)->print(os, p.domain().predicates(), p.terms(), 0, Bindings::EMPTY);
   }
+  for (ValueMap::const_iterator vi = p.init_values_.begin();
+       vi != p.init_values_.end(); vi++) {
+    os << std::endl << "  (= ";
+    (*vi).first->print(os, p.domain().functions(), p.terms());
+    os << ' ' << (*vi).second << ")";
+  }
   os << std::endl << "goal: ";
   p.goal().print(os, p.domain().predicates(), p.terms(), 0, Bindings::EMPTY);
+  os << std::endl << "metric: ";
+  p.metric().print(os, p.domain().functions(), p.terms());
   return os;
 }
