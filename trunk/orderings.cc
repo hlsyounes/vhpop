d7eba583f9ae859856f636cdd8dffe6fb43e3ade
lorens <lorens@414ba85e-7e40-11de-ae7d-69a7100e5098>
2003-09-18 21:48:45 +0000

    Changed from float to integer representation of simple temporal network to avoid precision errors.

--- trunk/orderings.cc
+++ trunk/orderings.cc
@@ -13,7 +13,7 @@
  * SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
  * ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
  *
- * $Id: orderings.cc,v 6.6 2003-09-08 21:27:52 lorens Exp $
+ * $Id: orderings.cc,v 6.7 2003-09-18 21:48:45 lorens Exp $
  */
 #include "orderings.h"
 #include "plans.h"
@@ -115,21 +115,21 @@ private:
 
 
 /* ====================================================================== */
-/* FloatVector */
+/* IntVector */
 
 /*
- * A collectible float vector.
+ * A collectible int vector.
  */
-struct FloatVector : public std::vector<float> {
+struct IntVector : public std::vector<int> {
   /* Register use of the given vector. */
-  static void register_use(const FloatVector* v) {
+  static void register_use(const IntVector* v) {
     if (v != NULL) {
       v->ref_count_++;
     }
   }
 
   /* Unregister use of the given vector. */
-  static void unregister_use(const FloatVector* v) {
+  static void unregister_use(const IntVector* v) {
     if (v != NULL) {
       v->ref_count_--;
       if (v->ref_count_ == 0) {
@@ -139,16 +139,16 @@ struct FloatVector : public std::vector<float> {
   }
 
   /* Constructs a vector with n copies of b. */
-  FloatVector(size_t n, float f)
-    : std::vector<float>(n, f), ref_count_(0) {
+  IntVector(size_t n, int f)
+    : std::vector<int>(n, f), ref_count_(0) {
 #ifdef DEBUG_MEMORY
     created_float_vectors++;
 #endif
   }
 
   /* Constructs a copy of the given vector. */
-  FloatVector(const FloatVector& v)
-    : std::vector<float>(v), ref_count_(0) {
+  IntVector(const IntVector& v)
+    : std::vector<int>(v), ref_count_(0) {
 #ifdef DEBUG_MEMORY
     created_float_vectors++;
 #endif
@@ -156,7 +156,7 @@ struct FloatVector : public std::vector<float> {
 
 #ifdef DEBUG_MEMORY
   /* Deletes this vector. */
-  ~FloatVector() {
+  ~IntVector() {
     deleted_float_vectors++;
   }
 #endif
@@ -499,16 +499,18 @@ void BinaryOrderings::print(std::ostream& os) const {
 /* TemporalOrderings */
 
 /* Constructs an empty ordering collection. */
-TemporalOrderings::TemporalOrderings() {}
+TemporalOrderings::TemporalOrderings()
+  : goal_achievers_(NULL) {}
 
 
 /* Constructs a copy of this ordering collection. */
 TemporalOrderings::TemporalOrderings(const TemporalOrderings& o)
-  : Orderings(o), distance_(o.distance_) {
+  : Orderings(o), distance_(o.distance_), goal_achievers_(o.goal_achievers_) {
   size_t n = distance_.size();
   for (size_t i = 0; i < n; i++) {
-    FloatVector::register_use(distance_[i]);
+    IntVector::register_use(distance_[i]);
   }
+  Chain<size_t>::register_use(goal_achievers_);
 }
 
 
@@ -516,8 +518,9 @@ TemporalOrderings::TemporalOrderings(const TemporalOrderings& o)
 TemporalOrderings::~TemporalOrderings() {
   size_t n = distance_.size();
   for (size_t i = 0; i < n; i++) {
-    FloatVector::unregister_use(distance_[i]);
+    IntVector::unregister_use(distance_[i]);
   }
+  Chain<size_t>::unregister_use(goal_achievers_);
 }
 
 
@@ -531,7 +534,7 @@ bool TemporalOrderings::possibly_before(size_t id1, StepTime t1,
   } else if (id1 == Plan::GOAL_ID || id2 == 0) {
     return false;
   } else {
-    return distance(time_node(id1, t1), time_node(id2, t2)) > threshold;
+    return distance(time_node(id1, t1), time_node(id2, t2)) > 0;
   }
 }
 
@@ -546,7 +549,7 @@ bool TemporalOrderings::possibly_after(size_t id1, StepTime t1,
   } else if (id1 == Plan::GOAL_ID || id2 == 0) {
     return true;
   } else {
-    return distance(time_node(id2, t2), time_node(id1, t1)) > threshold;
+    return distance(time_node(id2, t2), time_node(id1, t1)) > 0;
   }
 }
 
@@ -558,21 +561,62 @@ const TemporalOrderings* TemporalOrderings::refine(size_t step_id,
   if (step_id != 0 && step_id != Plan::GOAL_ID) {
     size_t i = time_node(step_id, STEP_START);
     size_t j = time_node(step_id, STEP_END);
-    if (-distance(i, 0) >= min_start && -distance(j, 0) >= min_end) {
+    int start = int(min_start/threshold + 0.5);
+    int end = int(min_end/threshold + 0.5);
+    if (-distance(i, 0) >= start && -distance(j, 0) >= end) {
       return this;
-    } else if (distance(0, i) < min_start || distance(0, j) < min_end) {
+    } else if (distance(0, i) < start || distance(0, j) < end) {
       return NULL;
     } else {
       TemporalOrderings& orderings = *new TemporalOrderings(*this);
-      std::map<size_t, FloatVector*> own_data;
-      if (orderings.fill_transitive(own_data, 0, i, min_start)
-	  && orderings.fill_transitive(own_data, 0, j, min_end)) {
+      std::map<size_t, IntVector*> own_data;
+      if (orderings.fill_transitive(own_data, 0, i, start)
+	  && orderings.fill_transitive(own_data, 0, j, end)) {
 	return &orderings;
       } else {
 	delete &orderings;
 	return NULL;
       }
     }
+  } else {
+    return this;
+  }
+}
+
+
+/* Returns the ordering collection with the given additions. */
+const TemporalOrderings*
+TemporalOrderings::refine(float time, const Step& new_step) const {
+  if (new_step.id() != 0 && new_step.id() != Plan::GOAL_ID
+      && new_step.id() > distance_.size()/2) {
+    int itime = int(time/threshold + 0.5);
+    TemporalOrderings& orderings = *new TemporalOrderings(*this);
+    IntVector* fv = new IntVector(4*new_step.id() - 2, INT_MAX);
+    /* Time for start of new step. */
+    (*fv)[0] = itime;
+    (*fv)[4*new_step.id() - 3] = -itime;
+    for (size_t id = 1; id < new_step.id(); id++) {
+      int t = itime - (*distance_[2*id - 1])[0];
+      (*fv)[2*id - 1] = (*fv)[2*id] = t;
+      (*fv)[4*new_step.id() - 2*id - 2] = -t;
+      (*fv)[4*new_step.id() - 2*id - 3] = -t;
+    }
+    orderings.distance_.push_back(fv);
+    IntVector::register_use(fv);
+    fv = new IntVector(4*new_step.id(), INT_MAX);
+    /* Time for end of new step. */
+    (*fv)[0] = itime;
+    (*fv)[4*new_step.id() - 1] = -itime;
+    for (size_t id = 1; id < new_step.id(); id++) {
+      int t = itime - (*distance_[2*id - 1])[0];
+      (*fv)[2*id - 1] = (*fv)[2*id] = t;
+      (*fv)[4*new_step.id() - 2*id] = -t;
+      (*fv)[4*new_step.id() - 2*id - 1] = -t;
+    }
+    (*fv)[2*new_step.id() - 1] = (*fv)[2*new_step.id()] = 0;
+    orderings.distance_.push_back(fv);
+    IntVector::register_use(fv);
+    return &orderings;
   } else {
     return this;
   }
@@ -587,10 +631,10 @@ TemporalOrderings::refine(const Ordering& new_ordering) const {
       && possibly_after(new_ordering.before_id(), new_ordering.before_time(),
 			new_ordering.after_id(), new_ordering.after_time())) {
     TemporalOrderings& orderings = *new TemporalOrderings(*this);
-    std::map<size_t, FloatVector*> own_data;
+    std::map<size_t, IntVector*> own_data;
     size_t i = time_node(new_ordering.before_id(), new_ordering.before_time());
     size_t j = time_node(new_ordering.after_id(), new_ordering.after_time());
-    if (orderings.fill_transitive(own_data, i, j, threshold)) {
+    if (orderings.fill_transitive(own_data, i, j, 1)) {
       return &orderings;
     } else {
       delete &orderings;
@@ -609,7 +653,7 @@ TemporalOrderings::refine(const Ordering& new_ordering,
 			  const Bindings* bindings) const {
   if (new_step.id() != 0 && new_step.id() != Plan::GOAL_ID) {
     TemporalOrderings& orderings = *new TemporalOrderings(*this);
-    std::map<size_t, FloatVector*> own_data;
+    std::map<size_t, IntVector*> own_data;
     if (new_step.id() > distance_.size()/2) {
       const Value* min_v =
 	dynamic_cast<const Value*>(&new_step.action().min_duration());
@@ -637,31 +681,40 @@ TemporalOrderings::refine(const Ordering& new_ordering,
       } else {
 	end_time = threshold + min_v->value();
       }
-      FloatVector* fv = new FloatVector(4*new_step.id() - 2, INFINITY);
+      IntVector* fv = new IntVector(4*new_step.id() - 2, INT_MAX);
       /* Earliest time for start of new step. */
-      (*fv)[4*new_step.id() - 3] = -start_time;
+      (*fv)[4*new_step.id() - 3] = -int(start_time/threshold + 0.5);
       own_data.insert(std::make_pair(orderings.distance_.size(), fv));
       orderings.distance_.push_back(fv);
-      FloatVector::register_use(fv);
-      fv = new FloatVector(4*new_step.id(), INFINITY);
+      IntVector::register_use(fv);
+      fv = new IntVector(4*new_step.id(), INT_MAX);
       /* Earliest time for end of new step. */
-      (*fv)[4*new_step.id() - 1] = -end_time;
-      (*fv)[2*new_step.id() - 1] = max_v->value();
-      (*fv)[2*new_step.id()] = -min_v->value();
+      (*fv)[4*new_step.id() - 1] = -int(end_time/threshold + 0.5);
+      if (!isinf(max_v->value())) {
+	(*fv)[2*new_step.id() - 1] = int(max_v->value()/threshold + 0.5);
+      }
+      (*fv)[2*new_step.id()] = -int(min_v->value()/threshold + 0.5);
       own_data.insert(std::make_pair(orderings.distance_.size(), fv));
       orderings.distance_.push_back(fv);
-      FloatVector::register_use(fv);
+      IntVector::register_use(fv);
     }
-    if (new_ordering.before_id() != 0
-	&& new_ordering.after_id() != Plan::GOAL_ID) {
-      size_t i = time_node(new_ordering.before_id(),
-			   new_ordering.before_time());
-      size_t j = time_node(new_ordering.after_id(), new_ordering.after_time());
-      if (orderings.fill_transitive(own_data, i, j, threshold)) {
-	return &orderings;
+    if (new_ordering.before_id() != 0) {
+      if (new_ordering.after_id() != Plan::GOAL_ID) {
+	size_t i = time_node(new_ordering.before_id(),
+			     new_ordering.before_time());
+	size_t j = time_node(new_ordering.after_id(),
+			     new_ordering.after_time());
+	if (orderings.fill_transitive(own_data, i, j, 1)) {
+	  return &orderings;
+	} else {
+	  delete &orderings;
+	  return NULL;
+	}
       } else {
-	delete &orderings;
-	return NULL;
+	orderings.goal_achievers_ =
+	  new Chain<size_t>(new_ordering.before_id(),
+			    orderings.goal_achievers_);
+	Chain<size_t>::register_use(orderings.goal_achievers_);
       }
     }
     return &orderings;
@@ -679,11 +732,12 @@ TemporalOrderings::schedule(std::map<size_t, float>& start_times,
   float max_dist = 0.0f;
   size_t n = distance_.size()/2;
   for (size_t i = 1; i <= n; i++) {
-    start_times.insert(std::make_pair(i,
-				      -distance(time_node(i, STEP_START), 0)));
-    float ed = -distance(time_node(i, STEP_END), 0);
+    float sd = -distance(time_node(i, STEP_START), 0)*threshold;
+    start_times.insert(std::make_pair(i, sd));
+    float ed = -distance(time_node(i, STEP_END), 0)*threshold;
     end_times.insert(std::make_pair(i, ed));
-    if (ed > max_dist) {
+    if (ed > max_dist
+	&& goal_achievers_ != NULL && goal_achievers_->contains(i)) {
       max_dist = ed;
     }
   }
@@ -698,8 +752,9 @@ TemporalOrderings::makespan(const std::map<std::pair<size_t, StepTime>,
   float max_dist = 0.0f;
   size_t n = distance_.size()/2;
   for (size_t i = 1; i <= n; i++) {
-    float ed = -distance(time_node(i, STEP_END), 0);
-    if (ed > max_dist) {
+    float ed = -distance(time_node(i, STEP_END), 0)*threshold;
+    if (ed > max_dist
+	&& goal_achievers_ != NULL && goal_achievers_->contains(i)) {
       max_dist = ed;
     }
   }
@@ -708,9 +763,9 @@ TemporalOrderings::makespan(const std::map<std::pair<size_t, StepTime>,
 
 
 /* Returns the maximum distance from the first and the second time node. */
-float TemporalOrderings::distance(size_t t1, size_t t2) const {
+int TemporalOrderings::distance(size_t t1, size_t t2) const {
   if (t1 == t2) {
-    return 0.0f;
+    return 0;
   } else if (t1 < t2) {
     return (*distance_[t2 - 1])[t1];
   } else {
@@ -720,20 +775,20 @@ float TemporalOrderings::distance(size_t t1, size_t t2) const {
 
 
 /* Sets the maximum distance from the first and the second time node. */
-void TemporalOrderings::set_distance(std::map<size_t, FloatVector*>& own_data,
-				     size_t t1, size_t t2, float d) {
+void TemporalOrderings::set_distance(std::map<size_t, IntVector*>& own_data,
+				     size_t t1, size_t t2, int d) {
   if (t1 != t2) {
     size_t i = std::max(t1, t2) - 1;
-    FloatVector* fv;
-    std::map<size_t, FloatVector*>::const_iterator vi = own_data.find(i);
+    IntVector* fv;
+    std::map<size_t, IntVector*>::const_iterator vi = own_data.find(i);
     if (vi != own_data.end()) {
       fv = (*vi).second;
     } else {
-      const FloatVector* old_fv = distance_[i];
-      fv = new FloatVector(*old_fv);
-      FloatVector::unregister_use(old_fv);
+      const IntVector* old_fv = distance_[i];
+      fv = new IntVector(*old_fv);
+      IntVector::unregister_use(old_fv);
       distance_[i] = fv;
-      FloatVector::register_use(fv);
+      IntVector::register_use(fv);
       own_data.insert(std::make_pair(i, fv));
     }
     if (t1 < t2) {
@@ -747,8 +802,8 @@ void TemporalOrderings::set_distance(std::map<size_t, FloatVector*>& own_data,
 
 /* Updates the transitive closure given a new ordering constraint. */
 bool
-TemporalOrderings::fill_transitive(std::map<size_t, FloatVector*>& own_data,
-				   size_t i, size_t j, float dist) {
+TemporalOrderings::fill_transitive(std::map<size_t, IntVector*>& own_data,
+				   size_t i, size_t j, int dist) {
   if (distance(j, i) > -dist) {
     /*
      * Update the temporal constraints.
@@ -757,13 +812,14 @@ TemporalOrderings::fill_transitive(std::map<size_t, FloatVector*>& own_data,
      */
     size_t n = distance_.size();
     for (size_t k = 0; k <= n; k++) {
-      float d_ik = distance(i, k);
-      if (!isinf(d_ik) && distance(j, k) > d_ik - dist) {
+      int d_ik = distance(i, k);
+      if (d_ik < INT_MAX && distance(j, k) > d_ik - dist) {
 	for (size_t l = 0; l <= n; l++) {
-	  float d_lj = distance(l, j);
-	  if (!isinf(d_lj) && distance(l, k) > d_ik + d_lj - dist) {
-	    set_distance(own_data, l, k, d_ik + d_lj - dist);
-	    if (-distance(k, l) > d_ik + d_lj - dist) {
+	  int d_lj = distance(l, j);
+	  int new_d = d_ik + d_lj - dist;
+	  if (d_lj < INT_MAX && distance(l, k) > new_d) {
+	    set_distance(own_data, l, k, new_d);
+	    if (-distance(k, l) > new_d) {
 	      return false;
 	    }
 	  }
@@ -781,8 +837,13 @@ void TemporalOrderings::print(std::ostream& os) const {
   for (size_t r = 0; r <= n; r++) {
     os << std::endl;
     for (size_t c = 0; c <= n; c++) {
-      os.width(8);
-      os << distance(r, c);
+      os.width(7);
+      int d = distance(r, c);
+      if (d < INT_MAX) {
+	os << d;
+      } else {
+	os << "inf";
+      }
     }
   }
 }
