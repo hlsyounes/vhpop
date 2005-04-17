6b35953e38149a354cb41a9052414f260d513d68
lorens <hlsyounes@gmail.com>
2005-04-17 11:19:39 +0000

    Functions are now stored in a single global table.

--- functions.h
+++ functions.h
@@ -1,22 +1,26 @@
 /* -*-C++-*- */
 /*
- * Functions.
+ * PDDL functions.
  *
- * Copyright (C) 2003 Carnegie Mellon University
- * Written by Håkan L. S. Younes.
+ * Copyright (C) 2002-2005 Carnegie Mellon University
  *
- * Permission is hereby granted to distribute this software for
- * non-commercial research purposes, provided that this copyright
- * notice is included with any such distribution.
+ * This file is part of VHPOP.
  *
- * THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
- * EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
- * SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
- * ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ * VHPOP is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * $Id: functions.h,v 6.1 2003-12-05 23:19:15 lorens Exp $
+ * VHPOP is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VHPOP; if not, write to the Free Software Foundation,
+ * Inc., #59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * $Id: functions.h,v 6.2 2005-04-17 11:19:26 lorens Exp $
  */
 #ifndef FUNCTIONS_H
 #define FUNCTIONS_H
@@ -25,19 +29,48 @@
 #include "types.h"
 #include <iostream>
 #include <map>
+#include <set>
 #include <string>
 #include <vector>
 
 
-/* Function index. */
-typedef int Function;
+/* ====================================================================== */
+/* Function */
 
-/* Name of number type. */
-const std::string NUMBER_NAME("number");
-/* The total-time function. */
-const Function TOTAL_TIME_FUNCTION = 0;
-/* Name of total-time function. */
-const std::string TOTAL_TIME_NAME("total-time");
+/*
+ * A function.
+ */
+struct Function {
+  /* Constructs a function. */
+  explicit Function(int index) : index_(index) {}
+
+private:
+  /* Function index. */
+  int index_;
+
+  friend bool operator==(const Function& f1, const Function& f2);
+  friend bool operator<(const Function& f1, const Function& f2);
+  friend std::ostream& operator<<(std::ostream& os, const Function& f);
+  friend struct FunctionTable;
+};
+
+/* Equality operator for functions. */
+inline bool operator==(const Function& f1, const Function& f2) {
+  return f1.index_ == f2.index_;
+}
+
+/* Inequality operator for functions. */
+inline bool operator!=(const Function& f1, const Function& f2) {
+  return !(f1 == f2);
+}
+
+/* Less-than operator for functions. */
+inline bool operator<(const Function& f1, const Function& f2) {
+  return f1.index_ < f2.index_;
+}
+
+/* Output operator for functions. */
+std::ostream& operator<<(std::ostream& os, const Function& f);
 
 
 /* ====================================================================== */
@@ -55,66 +88,46 @@ struct FunctionSet : public std::set<Function> {
  * Function table.
  */
 struct FunctionTable {
-  /* Adds a function with the given name to this table and returns the
-     function. */
-  Function add_function(const std::string& name);
-
-  /* Returns the function with the given name.  If no function with
-     the given name exists, false is returned in the second part of
-     the result. */
-  std::pair<Function, bool> find_function(const std::string& name) const;
-
-  /* Returns the first function of this function table. */
-  Function first_function() const { return TOTAL_TIME_FUNCTION; }
-
-  /* Returns the last function of this function table. */
-  Function last_function() const { return names_.size(); }
-
   /* Adds a parameter with the given type to the given function. */
-  void add_parameter(Function function, Type type) {
-    parameters_[function - 1].push_back(type);
-  }
+  static void add_parameter(const Function& function, const Type& type);
 
   /* Returns the name of the given function. */
-  const std::string& name(Function function) const {
-    return ((function == TOTAL_TIME_FUNCTION)
-	    ? TOTAL_TIME_NAME : names_[function - 1]);
-  }
+  static const std::string& name(const Function& function);
 
-  /* Returns the arity of the given function. */
-  size_t arity(Function function) const {
-    return ((function == TOTAL_TIME_FUNCTION)
-	    ? 0 : parameters_[function - 1].size());
-  }
-
-  /* Returns the ith parameter type of the given function. */
-  Type parameter(Function function, size_t i) const {
-    return parameters_[function - 1][i];
-  }
+  /* Returns the parameter types of the given function. */
+  static const TypeList& parameters(const Function& function);
 
   /* Makes the given function dynamic. */
-  void make_dynamic(Function function) {
-    static_functions_.erase(function);
-  }
+  static void make_dynamic(const Function& function);
 
   /* Tests if the given function is static. */
-  bool static_function(Function function) const {
-    return static_functions_.find(function) != static_functions_.end();
-  }
+  static bool static_function(const Function& function);
 
-  /* Prints the given function on the given stream. */
-  void print_function(std::ostream& os, Function function) const;
+  /* Adds a function with the given name to this table and returns the
+     function. */
+  const Function& add_function(const std::string& name);
+
+  /* Returns a pointer to the function with the given name, or 0 if no
+     function with the given name exists. */
+  const Function* find_function(const std::string& name) const;
 
 private:
   /* Function names. */
-  std::vector<std::string> names_;
+  static std::vector<std::string> names_;
+  /* Function parameters. */
+  static std::vector<TypeList> parameters_;
+  /* Static functions. */
+  static FunctionSet static_functions_;
+
   /* Mapping of function names to functions. */
   std::map<std::string, Function> functions_;
-  /* Function parameters. */
-  std::vector<TypeList> parameters_;
-  /* Static functions. */
-  FunctionSet static_functions_;
+
+  friend std::ostream& operator<<(std::ostream& os, const FunctionTable& t);
+  friend std::ostream& operator<<(std::ostream& os, const Function& f);
 };
+
+/* Output operator for function tables. */
+std::ostream& operator<<(std::ostream& os, const FunctionTable& t);
 
 
 #endif /* FUNCTIONS_H */
