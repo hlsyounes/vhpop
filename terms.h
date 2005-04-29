7744e084d2e569f664f94dac123185f33bba7840
lorens <hlsyounes@gmail.com>
2005-04-29 09:30:22 +0000

    Terms are now stored in a single global table.

--- terms.h
+++ terms.h
@@ -1,22 +1,26 @@
 /* -*-C++-*- */
 /*
- * Terms.
+ * PDDL terms.
  *
- * Copyright (C) 2003 Carnegie Mellon University
- * Written by Håkan L. S. Younes.
+ * Copyright (C) 2002-2005 Carnegie Mellon University
  *
- * Permission is hereby granted to distribute this software for
- * non-commercial research purposes, provided that this copyright
- * notice is included with any such distribution.
+ * This file is part of VHPOP.
  *
- * THIS SOFTWARE IS PROVIDED "AS IS" WITHOUT WARRANTY OF ANY KIND,
- * EITHER EXPRESSED OR IMPLIED, INCLUDING, BUT NOT LIMITED TO, THE
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
- * PURPOSE.  THE ENTIRE RISK AS TO THE QUALITY AND PERFORMANCE OF THE
- * SOFTWARE IS WITH YOU.  SHOULD THE PROGRAM PROVE DEFECTIVE, YOU
- * ASSUME THE COST OF ALL NECESSARY SERVICING, REPAIR OR CORRECTION.
+ * VHPOP is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
  *
- * $Id: terms.h,v 6.4 2003-09-05 16:33:35 lorens Exp $
+ * VHPOP is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with VHPOP; if not, write to the Free Software Foundation,
+ * Inc., #59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+ *
+ * $Id: terms.h,v 6.5 2005-04-29 09:30:02 lorens Exp $
  */
 #ifndef TERMS_H
 #define TERMS_H
@@ -28,24 +32,104 @@
 #include <string>
 #include <vector>
 
-struct Bindings;
+
+/* ====================================================================== */
+/* Object */
+
+struct Term;
+
+/*
+ * An object.
+ */
+struct Object {
+  /* Constructs an object. */
+  explicit Object(int index) : index_(index) {}
+
+  /* Converts this object to a term. */
+  operator Term() const;
+
+private:
+  /* Object index. */
+  int index_;
+};
 
 
-/* Term index. */
-typedef int Term;
-/* Object index. */
-typedef size_t Object;
-/* Variable index. */
-typedef int Variable;
+/* ====================================================================== */
+/* Variable */
 
-/* The "null" term. */
-const Term NULL_TERM = 0;
+/*
+ * A variable.
+ */
+struct Variable {
+  /* Constructs a variable. */
+  explicit Variable(int index) : index_(index) {}
 
-/* Tests if the given term is an object. */
-inline bool is_object(Term term) { return term > 0; }
+  /* Converts this variable to a term. */
+  operator Term() const;
 
-/* Tests if the given term is a variable. */
-inline bool is_variable(Term term) { return term < 0; }
+private:
+  /* Variable index. */
+  int index_;
+};
+
+
+/* ====================================================================== */
+/* Term */
+
+/*
+ * A term.
+ */
+struct Term {
+  /* Constructs a term. */
+  explicit Term(int index) : index_(index) {}
+
+  /* Tests if this term is an object. */
+  bool object() const { return index_ >= 0; }
+
+  /* Tests if this term is a variable. */
+  bool variable() const { return index_ < 0; }
+
+  /* Converts this term to an object.  Fails if the term is not an
+     object. */
+  Object as_object() const;
+
+  /* Converts this term to a variable.  Fails if the term is not a
+     variable. */
+  Variable as_variable() const;
+
+private:
+  /* Term index. */
+  int index_;
+
+  friend bool operator==(const Term& t1, const Term& t2);
+  friend bool operator<(const Term& t1, const Term& t2);
+  friend std::ostream& operator<<(std::ostream& os, const Term& t);
+  friend struct TermTable;
+};
+
+/* Converts this object to a term. */
+inline Object::operator Term() const { return Term(index_); }
+
+/* Converts this variable to a term. */
+inline Variable::operator Term() const { return Term(index_); }
+
+/* Equality operator for terms. */
+inline bool operator==(const Term& t1, const Term& t2) {
+  return t1.index_ == t2.index_;
+}
+
+/* Inequality operator for terms. */
+inline bool operator!=(const Term& t1, const Term& t2) {
+  return !(t1 == t2);
+}
+
+/* Less-than operator for terms. */
+inline bool operator<(const Term& t1, const Term& t2) {
+  return t1.index_ < t2.index_;
+}
+
+/* Output operator for terms. */
+std::ostream& operator<<(std::ostream& os, const Term& t);
 
 
 /* ====================================================================== */
@@ -54,7 +138,7 @@ inline bool is_variable(Term term) { return term < 0; }
 /*
  * Variable substitutions.
  */
-struct SubstitutionMap : public std::map<Variable, Object> {
+struct SubstitutionMap : public std::map<Variable, Term> {
 };
 
 
@@ -95,69 +179,57 @@ struct VariableList : public std::vector<Variable> {
  * Term table.
  */
 struct TermTable {
+  /* Returns a fresh variable with the given type. */
+  static Variable add_variable(const Type& type);
+
+  /* Sets the type of the given term. */
+  static void set_type(const Term& term, const Type& type);
+
+  /* Returns the type of the given term. */
+  static const Type& type(const Term& term);
+
   /* Constructs an empty term table. */
-  TermTable() : parent_(NULL) {}
+  TermTable() : parent_(0) {}
+
+  /* Deletes this term table. */
+  ~TermTable();
 
   /* Constructs a term table extending the given term table. */
   explicit TermTable(const TermTable& parent) : parent_(&parent) {}
 
   /* Adds an object with the given name and type to this term table
      and returns the object. */
-  Object add_object(const std::string& name, Type type);
+  const Object& add_object(const std::string& name, const Type& type);
 
-  /* Adds a variable with the given name and type to this term table
-     and returns the variable. */
-  Variable add_variable(Type type);
+  /* Returns the object with the given name, or 0 if no object with
+     the given name exists. */
+  const Object* find_object(const std::string& name) const;
 
-  /* Returns the object with the given name. If no object with the
-     given name exists, false is returned in the second part of the
-     result. */
-  std::pair<Object, bool> find_object(const std::string& name) const;
-
-  /* Returns the first object of this term table. */
-  Object first_object() const {
-    return (parent_ != NULL) ? parent_->last_object() + 1 : 1;
-  }
-
-  /* Returns the last object of this term table. */
-  Object last_object() const { return first_object() + names_.size() - 1; }
-
-  /* Returns the first variable of this term table. */
-  Variable first_variable() const {
-    return (parent_ != NULL) ? parent_->last_variable() - 1 : -1;
-  }
-
-  /* Returns the last variable of this term table. */
-  Variable last_variable() const {
-    return first_variable() - variable_types_.size() + 1;
-  }
-
-  /* Sets the type of the given term. */
-  void set_type(Term term, Type type);
-
-  /* Returns the type of the given term. */
-  Type type(Term term) const;
-
-  /* Prints the given term on the given stream. */
-  void print_term(std::ostream& os, Term term) const;
-
-  /* Prints the given term on the given stream subject to the given
-     bindings. */
-  void print_term(std::ostream& os, Term term, size_t step_id,
-		  const Bindings& bindings) const;
+  /* Returns a list with objects that are compatible with the given
+     type. */
+  const ObjectList& compatible_objects(const Type& type) const;
 
 private:
+  /* Object names. */
+  static std::vector<std::string> names_;
+  /* Object types. */
+  static TypeList object_types_;
+  /* Variable types. */
+  static TypeList variable_types_;
+
   /* Parent term table. */
   const TermTable* parent_;
-  /* Object names. */
-  std::vector<std::string> names_;
   /* Mapping of object names to objects. */
   std::map<std::string, Object> objects_;
-  /* Object types. */
-  TypeList object_types_;
-  /* Variable types. */
-  TypeList variable_types_;
+  /* Cached results of compatible objects queries. */
+  mutable std::map<Type, const ObjectList*> compatible_;
+
+  friend std::ostream& operator<<(std::ostream& os, const TermTable& t);
+  friend std::ostream& operator<<(std::ostream& os, const Term& t);
 };
+
+/* Output operator for term tables. */
+std::ostream& operator<<(std::ostream& os, const TermTable& t);
 
 
 #endif /* TERMS_H */
